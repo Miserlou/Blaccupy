f4170640c417a558afabaabbdec611f58e8669b2
brendanarnold <brendanarnold@gmail.com>
2012-01-12 18:23:21 +0000

    Added time dependent bit ...

--- blaccupy.js
+++ blaccupy.js
@@ -1,10 +1,15 @@
 // Rich Jones of Gun.io, public domain. Fork me, please! http://github.com/Miserlou/
-$(document).ready(function() {
-		$('body').css('background', '#000000');
-		$('body').css('text-align', 'center');
-		$('body').css('color', '#111111');
-		$('body').css('font-family','Helvetica');
-		$('body').css('font-size','2.2em');
-		$('body').css('vertical-align','middle');
-		$('body').html('<div style="width: 960px; margin: 0 auto;"><br /><br />This website is..<br /> <br /><div style="color: #222222">BLACKED OUT!</div><br />..in protest of <a href="http://en.wikipedia.org/wiki/Stop_Online_Piracy_Act" target="_blank" style="color: #220022">pending legislation</a> which threatens the freedoms of websites like this one and the freedoms of the people who use them.<br /><br />Please <a href="http://americancensorship.org/" target="_blank" style="color: #220022">help protect our free speech</a> against the corporate and political interests which seek to take them away!<br /><br />(And don\'t worry, we\'ll be back in business tomorrow!)</div>');
+now = new Date();
+
+if (now.getDate() == 18 && now.getMonth() == 0 && now.getFullYear() == 2012) {
+
+	$(document).ready(function() {
+			$('body').css('background', '#000000');
+			$('body').css('text-align', 'center');
+			$('body').css('color', '#111111');
+			$('body').css('font-family','Helvetica');
+			$('body').css('font-size','2.2em');
+			$('body').css('vertical-align','middle');
+			$('body').html('<div style="width: 960px; margin: 0 auto;"><br /><br />This website is..<br /> <br /><div style="color: #222222">BLACKED OUT!</div><br />..in protest of <a href="http://en.wikipedia.org/wiki/Stop_Online_Piracy_Act" target="_blank" style="color: #220022">pending legislation</a> which threatens the freedoms of websites like this one and the freedoms of the people who use them.<br /><br />Please <a href="http://americancensorship.org/" target="_blank" style="color: #220022">help protect our free speech</a> against the corporate and political interests which seek to take them away!<br /><br />(And don\'t worry, we\'ll be back in business tomorrow!)</div>');
 	});
+}
